e5066f45e6840e08784058b09a8b990aa6a58cb1
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-01-04 13:27:58 +0530

    [MR.3.18] Deleting a Movie

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -6,6 +6,12 @@ export default class Movies extends Component {
 	state = {
 		movies: getMovies(),
 	};
+
+	handleDelete = (movie) => {
+		const movies = this.state.movies.filter((m) => m._id !== movie._id);
+		this.setState({ movies });
+	};
+
 	render() {
 		return (
 			<div>
@@ -16,15 +22,24 @@ export default class Movies extends Component {
 							<th>Genre</th>
 							<th>Stock</th>
 							<th>Rate</th>
+							<th>Remove</th>
 						</tr>
 					</thead>
 					<tbody>
 						{this.state.movies.map((movie) => (
-							<tr>
+							<tr key={movie._id}>
 								<td>{movie.title}</td>
 								<td>{movie.genre.name}</td>
 								<td>{movie.numberInStock}</td>
 								<td>{movie.dailyRentalRate}</td>
+								<td>
+									<button
+										onClick={() => this.handleDelete(movie)}
+										className="btn btn-danger btn-sm"
+									>
+										Delete
+									</button>
+								</td>
 							</tr>
 						))}
 					</tbody>
