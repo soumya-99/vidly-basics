cda1c8790cc6ea460574f4694213416cee8c918c
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-01-04 13:15:56 +0530

    [MR.3.17] Building The Movies Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Movies from './components/Movies';
 
 function App() {
   return (
-    <main className="container">
-      <h1>Hello World</h1>
-    </main>
+    <div className="container">
+      <Movies />
+    </div>
   );
 }
 
