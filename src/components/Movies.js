3f5c07d6d7ce8187f1cf70c4e16d9990d6b71441
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-01-06 15:45:10 +0530

    [MR.5.9] Filering Component Interface

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,12 +4,19 @@ import Like from "./common/Like"
 import { getMovies } from "../services/fakeMovieService"
 import Pagination from "./common/Pagination"
 import { paginate } from "../utils/paginate"
+import ListGroup from "./common/ListGroup"
+import { getGenres } from "../services/fakeGenreService"
 
 export default class Movies extends Component {
 	state = {
-		movies: getMovies(),
+		movies: [],
+		genres: [],
 		pageSize: 4,
 		currentPage: 1,
+	}
+
+	componentDidMount() {
+		this.setState({ movies: getMovies(), genres: getGenres() })
 	}
 
 	handleDelete = (movie) => {
@@ -29,6 +36,10 @@ export default class Movies extends Component {
 		this.setState({ currentPage: page })
 	}
 
+	handleGenreSelect = (genre) => {
+		console.log(genre);
+	}
+
 	render() {
 		const { length: count } = this.state.movies
 		const { pageSize, currentPage, movies: allMovies } = this.state
@@ -38,50 +49,56 @@ export default class Movies extends Component {
 		const movies = paginate(allMovies, currentPage, pageSize)
 
 		return (
-			<div>
-				<p>Showing {count} movies in the Database</p>
-				<table className="table">
-					<thead>
-						<tr>
-							<th>Title</th>
-							<th>Genre</th>
-							<th>Stock</th>
-							<th>Rate</th>
-							<th>Like</th>
-							<th>Remove</th>
-						</tr>
-					</thead>
-					<tbody>
-						{movies.map((movie) => (
-							<tr key={movie._id}>
-								<td>{movie.title}</td>
-								<td>{movie.genre.name}</td>
-								<td>{movie.numberInStock}</td>
-								<td>{movie.dailyRentalRate}</td>
-								<td>
-									<Like
-										liked={movie.liked}
-										onClick={() => this.handleLike(movie)}
-									/>
-								</td>
-								<td>
-									<button
-										onClick={() => this.handleDelete(movie)}
-										className="btn btn-danger btn-sm"
-									>
-										Delete
-									</button>
-								</td>
+			<div className="row">
+				<div className="col-2">
+					<ListGroup items={this.state.genres} onItemSelect={this.handleGenreSelect} />
+				</div>
+
+				<div className="col">
+					<p>Showing {count} movies in the Database</p>
+					<table className="table">
+						<thead>
+							<tr>
+								<th>Title</th>
+								<th>Genre</th>
+								<th>Stock</th>
+								<th>Rate</th>
+								<th>Like</th>
+								<th>Remove</th>
 							</tr>
-						))}
-					</tbody>
-				</table>
-				<Pagination
-					itemsCount={count}
-					pageSize={pageSize}
-					currentPage={currentPage}
-					onPageChange={this.handlePageChange}
-				/>
+						</thead>
+						<tbody>
+							{movies.map((movie) => (
+								<tr key={movie._id}>
+									<td>{movie.title}</td>
+									<td>{movie.genre.name}</td>
+									<td>{movie.numberInStock}</td>
+									<td>{movie.dailyRentalRate}</td>
+									<td>
+										<Like
+											liked={movie.liked}
+											onClick={() => this.handleLike(movie)}
+										/>
+									</td>
+									<td>
+										<button
+											onClick={() => this.handleDelete(movie)}
+											className="btn btn-danger btn-sm"
+										>
+											Delete
+										</button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+					<Pagination
+						itemsCount={count}
+						pageSize={pageSize}
+						currentPage={currentPage}
+						onPageChange={this.handlePageChange}
+					/>
+				</div>
 			</div>
 		)
 	}
---
+++ src/components/common/ListGroup.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ListGroup() {
+    return (
+        null
+    )
+}
+
+export default ListGroup
