378c8fafa62e56b32725106c82e1d3e4d385d8bf
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-01-06 20:18:35 +0530

    [MR.5.15] Moved movies table in a new component named MoviesTable

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react"
 
-import Like from "./common/Like"
 import { getMovies } from "../services/fakeMovieService"
 import Pagination from "./common/Pagination"
 import { paginate } from "../utils/paginate"
 import ListGroup from "./common/ListGroup"
 import { getGenres } from "../services/fakeGenreService"
+import MoviesTable from "./MoviesTable"
 
 export default class Movies extends Component {
 	state = {
@@ -71,42 +71,13 @@ export default class Movies extends Component {
 
 				<div className="col">
 					<p>Showing {filtered.length} movies in the Database</p>
-					<table className="table">
-						<thead>
-							<tr>
-								<th>Title</th>
-								<th>Genre</th>
-								<th>Stock</th>
-								<th>Rate</th>
-								<th>Like</th>
-								<th>Remove</th>
-							</tr>
-						</thead>
-						<tbody>
-							{movies.map((movie) => (
-								<tr key={movie._id}>
-									<td>{movie.title}</td>
-									<td>{movie.genre.name}</td>
-									<td>{movie.numberInStock}</td>
-									<td>{movie.dailyRentalRate}</td>
-									<td>
-										<Like
-											liked={movie.liked}
-											onClick={() => this.handleLike(movie)}
-										/>
-									</td>
-									<td>
-										<button
-											onClick={() => this.handleDelete(movie)}
-											className="btn btn-danger btn-sm"
-										>
-											Delete
-										</button>
-									</td>
-								</tr>
-							))}
-						</tbody>
-					</table>
+
+					<MoviesTable
+						movies={movies}
+						onLike={this.handleLike}
+						onDelete={this.handleDelete}
+					/>
+
 					<Pagination
 						itemsCount={filtered.length}
 						pageSize={pageSize}
---
+++ src/components/MoviesTable.js
@@ -0,0 +1,45 @@
+import React from "react"
+import Like from "./common/Like"
+
+function MoviesTable({ movies, onDelete, onLike }) {
+	return (
+		<table className="table">
+			<thead>
+				<tr>
+					<th>Title</th>
+					<th>Genre</th>
+					<th>Stock</th>
+					<th>Rate</th>
+					<th>Like</th>
+					<th>Remove</th>
+				</tr>
+			</thead>
+			<tbody>
+				{movies.map((movie) => (
+					<tr key={movie._id}>
+						<td>{movie.title}</td>
+						<td>{movie.genre.name}</td>
+						<td>{movie.numberInStock}</td>
+						<td>{movie.dailyRentalRate}</td>
+						<td>
+							<Like
+								liked={movie.liked}
+								onClick={() => onLike(movie)}
+							/>
+						</td>
+						<td>
+							<button
+								onClick={() => onDelete(movie)}
+								className="btn btn-danger btn-sm"
+							>
+								Delete
+							</button>
+						</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
+	)
+}
+
+export default MoviesTable
