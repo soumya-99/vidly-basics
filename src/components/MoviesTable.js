4961daa42ab56bf075d87f689f1bc6e1c1c0b757
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-01-17 19:48:57 +0530

    [MR.5.19] Extracting TableHeader

--- src/components/MoviesTable.js
+++ src/components/MoviesTable.js
@@ -1,32 +1,26 @@
 import React, { Component } from "react"
 import Like from "./common/Like"
+import TableHeader from "./common/TableHeader"
 
 export default class MoviesTable extends Component {
-	raiseSort = (path) => {
-		const sortColumn = { ...this.props.sortColumn }
-		if (sortColumn.path === path) {
-			sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc"
-		} else {
-			sortColumn.path = path
-			sortColumn.order = "asc"
-		}
-		this.props.onSort(sortColumn)
-	}
+	columns = [
+		{ path: "title", label: "Title" },
+		{ path: "genre.name", label: "Genre" },
+		{ path: "numberInStock", label: "Stock" },
+		{ path: "dailtRentalRate", label: "Rate" },
+		{ key: 'like', label: "Like" },
+		{ key: 'delete', label: "Delete" },
+	]
 
 	render() {
-		const { movies, onDelete, onLike } = this.props
+		const { movies, onDelete, onLike, sortColumn, onSort } = this.props
 		return (
 			<table className="table">
-				<thead>
-					<tr>
-						<th onClick={() => this.raiseSort("title")}>Title</th>
-						<th onClick={() => this.raiseSort("genre.name")}>Genre</th>
-						<th onClick={() => this.raiseSort("numberInStock")}>Stock</th>
-						<th onClick={() => this.raiseSort("dailyRate")}>Rate</th>
-						<th>Like</th>
-						<th>Remove</th>
-					</tr>
-				</thead>
+				<TableHeader
+					columns={this.columns}
+					sortColumn={sortColumn}
+					onSort={onSort}
+				/>
 				<tbody>
 					{movies.map((movie) => (
 						<tr key={movie._id}>
