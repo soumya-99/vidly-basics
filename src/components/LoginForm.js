3d06fc266dbf591aa0d3177bef9f679b14498385
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-03-20 21:31:00 +0530

    [MR.7.5] Controlled Elements (Like input field data synced with the local state)

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,30 +1,44 @@
-import React, { Component, createRef } from 'react'
+import React, { Component, createRef } from "react"
 
 export default class LoginForm extends Component {
+	state = {
+		account: { username: "", password: "" },
+	}
 
-    // userName = createRef()
+	handleSubmit = (e) => {
+		e.preventDefault()
 
-    // componentDidMount() {
-    //     this.userName.current.focus()
-    // }
-    
+		// Call the server
+		console.log("Submitted")
+	}
 
-    handleSubmit = e => {
-        e.preventDefault();
-
-        // Call the server
-        console.log("Submitted");
-    }
-    render() {
-        return (
-            <div>
-                <h1>Login</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <div className="form-group"><label htmlFor="username">Username</label><input autoFocus ref={this.userName} id="username" type="text" className="form-control"/></div>
-                    <div className="form-group"><label htmlFor="password">Password</label><input id="password" type="text" className="form-control"/></div>
-                    <button className="btn btn-primary">Login</button>
-                </form>
-            </div>
-        )
-    }
+	handleChange = (e) => {
+		const account = { ...this.state.account }
+		account.username = e.currentTarget.value
+		this.setState({ account })
+	}
+	render() {
+		return (
+			<div>
+				<h1>Login</h1>
+				<form onSubmit={this.handleSubmit}>
+					<div className="form-group">
+						<label htmlFor="username">Username</label>
+						<input
+							value={this.state.account.username}
+							onChange={this.handleChange}
+							id="username"
+							type="text"
+							className="form-control"
+						/>
+					</div>
+					<div className="form-group">
+						<label htmlFor="password">Password</label>
+						<input id="password" type="text" className="form-control" />
+					</div>
+					<button className="btn btn-primary">Login</button>
+				</form>
+			</div>
+		)
+	}
 }
