2b20d65887a52e18dc6e88e95a9cff10514b3c68
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-03-20 21:13:28 +0530

    [MR.7.3] Handling Form Submission and preventDefault()

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 
 export default class LoginForm extends Component {
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        // Call the server
+        console.log("Submitted");
+    }
     render() {
         return (
             <div>
                 <h1>Login</h1>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div className="form-group"><label htmlFor="username">Username</label><input id="username" type="text" className="form-control"/></div>
                     <div className="form-group"><label htmlFor="password">Password</label><input id="password" type="text" className="form-control"/></div>
                     <button className="btn btn-primary">Login</button>
