3283993ac72a0e1e9b363907ae22b70fc5ff33a1
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-03-20 21:20:36 +0530

    [MR.7.4] Working with refs

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 
 export default class LoginForm extends Component {
+
+    // userName = createRef()
+
+    // componentDidMount() {
+    //     this.userName.current.focus()
+    // }
+    
 
     handleSubmit = e => {
         e.preventDefault();
@@ -13,7 +20,7 @@ export default class LoginForm extends Component {
             <div>
                 <h1>Login</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <div className="form-group"><label htmlFor="username">Username</label><input id="username" type="text" className="form-control"/></div>
+                    <div className="form-group"><label htmlFor="username">Username</label><input autoFocus ref={this.userName} id="username" type="text" className="form-control"/></div>
                     <div className="form-group"><label htmlFor="password">Password</label><input id="password" type="text" className="form-control"/></div>
                     <button className="btn btn-primary">Login</button>
                 </form>
