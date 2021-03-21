1d590442eab75fc7b48b21c8d841cbf60376551d
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-03-21 21:00:03 +0530

    [MR.7.14] Validating a form using Joi and some little changes to write more efficient code

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -9,26 +9,21 @@ export default class LoginForm extends Component {
 	}
 
 	schema = {
-		username: Joi.string().required(),
-		password: Joi.string().required()
+		username: Joi.string().required().label("Username"),
+		password: Joi.string().required().label("Password"),
 	}
 
 	validate = () => {
+		const options = { abortEarly: false }
+		const { error } = Joi.validate(this.state.account, this.schema, options)
 
-
-		const result = Joi.validate(this.state.account, this.schema, {abortEarly: false})
-		console.log(result);
-
-
+		if (!error) return null
 
 		const errors = {}
-		// const { errors } = this.state
-
-		const { account } = this.state
-		if (account.username.trim() === "") errors.username = "Username is required"
-		if (account.password.trim() === "") errors.password = "Password is required"
-
-		return Object.keys(errors).length === 0 ? null : errors
+		for (let item of error.details) {
+			errors[item.path[0]] = item.message
+		}
+		return errors
 	}
 
 	handleSubmit = (e) => {
